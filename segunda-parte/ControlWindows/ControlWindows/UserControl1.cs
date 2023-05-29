e72d6a0414f73f33929b34bff7c0cc1a181cd6d0
fbnmlnss <fabimilanes@gmail.com>
2023-05-29 08:41:30 -0600

    se siguieron los pasos de la segunda parte del proyecto

---
+++ segunda-parte/ControlWindows/ControlWindows/UserControl1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlWindows
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "Hello World";
+        }
+    }
+}
