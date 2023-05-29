380a0d11717f092f6aa41651b0431681c3fdefc9
fbnmlnss <fabimilanes@gmail.com>
2023-05-28 19:18:49 -0600

    se siguieron los pasos de la primera parte del laboratorio

---
+++ primera-parte/MyTestingApp/src/mytestingapp/MyTestingApp.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package mytestingapp;
+
+/**
+ *
+ * @author fa-bi
+ */
+public class MyTestingApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MyForm myForm = new MyForm();
+        myForm.setVisible(true);
+    }
+}
